b3da5144b08b571be775b9d244caac5b68066ce7
Guy Routledge <guy@bigfish.co.uk>
2013-03-07 11:12:03 +0000

    Fix some JS bugs and tidy up the code
    Add section headings
    Fix no more widows snippet
    Remove redundant orientation change plugin
    General tidy up

--- wp-content/themes/guyroutledge-v1/js/scripts.js
+++ wp-content/themes/guyroutledge-v1/js/scripts.js
@@ -2,250 +2,179 @@
 
 jQuery(function($){
 
-	// Scroll the page between sections
+//*************************
+// $$ Scroll to Comments
+//**************************
+
 	if ( $('#respond').length ) {
 
 		$('[href="#respond"]').click(function(){
-
-		var target = $(this).attr('href');
-		var position = $(target).offset().top;
-
-		$('html, body').animate({
-			scrollTop : position
-		}, 500);
-
-		return false;
-
+			var target = $(this).attr('href');
+			var position = $(target).offset().top;
+			$('html, body').animate({
+				scrollTop : position
+			}, 500);
+			return false;
 		});
 	}
 
-  if ( $('html').height() > 1500 ) {
+//*************************
+// $$ Add Back to Top Link
+//**************************
 
-    $('footer .inner-wrap').append('<a class="to-top fr" href="#">to the top ^</a>');
+	if ( $('html').height() > 1500 ) {
 
-    $('.to-top').click(function(){
+		$('footer .inner-wrap').append('<a class="to-top fr" href="#">to the top ^</a>');
+		$('.to-top').click(function(){
+			$('html, body').animate({
+			scrollTop : 0
+			}, 500);
+			return false;
+		});
 
-    $('html, body').animate({
-      scrollTop : 0
-    }, 500);
+	}
 
-    return false;
+//*************************
+// $$ No More Widows
+//**************************
 
-    });
-  }
-	/* No more widows */
-	$('p').each(function(i, html) {
-		return html.replace(/\s([\S]+)$/,'&nbsp;$1'));
+	$('p').html(function(i, html) {
+		return html.replace(/\s([\S]+)$/,'&nbsp;$1');
 	});
 
-	// grab some tweets
 
-	JQTWEET = {
+//*************************
+// $$ JQTweet
+//**************************
 
-    // Set twitter username, number of tweets & id/class to append tweets
-    user: 'guyroutledge',
-    numTweets: 1,
-    appendTo: '#twitter',
+	var JQTWEET = {
 
-    // core function of jqtweet
-    loadTweets: function() {
-        $.ajax({
-            url: 'http://api.twitter.com/1/statuses/user_timeline.json/',
-            type: 'GET',
-            dataType: 'jsonp',
-            data: {
-                screen_name: JQTWEET.user,
-                include_rts: true,
-                count: JQTWEET.numTweets,
-                include_entities: true
-            },
-            success: function(data, textStatus, xhr) {
+	// Set twitter username, number of tweets & id/class to append tweets
+	user: 'guyroutledge',
+	numTweets: 1,
+	appendTo: '#twitter',
 
-                 var html = '<p>TWEET_TEXT</p><span class="tweet-meta">AGO</span>';
+	// core function of jqtweet
+	loadTweets: function() {
+		$.ajax({
+			url: 'http://api.twitter.com/1/statuses/user_timeline.json/',
+			type: 'GET',
+			dataType: 'jsonp',
+			data: {
+				screen_name: JQTWEET.user,
+				include_rts: true,
+				count: JQTWEET.numTweets,
+				include_entities: true
+			},
+			success: function(data, textStatus, xhr) {
 
-                 // append tweets into page
-                 for (var i = 0; i < data.length; i++) {
-                    $(JQTWEET.appendTo).append(
-                        html.replace('TWEET_TEXT', JQTWEET.ify.clean(data[i].text) )
-                            .replace(/USER/g, data[i].user.screen_name)
-                            .replace('AGO', JQTWEET.timeAgo(data[i].created_at) )
-                            .replace(/ID/g, data[i].id_str)
-                    );
-                 }
-            }
+					var html = '<p>TWEET_TEXT</p><span class="tweet-meta">AGO</span>';
 
-        });
+					// append tweets into page
+					for (var i = 0; i < data.length; i++) {
+					$(JQTWEET.appendTo).append(
+						html.replace('TWEET_TEXT', JQTWEET.ify.clean(data[i].text) )
+							.replace(/USER/g, data[i].user.screen_name)
+							.replace('AGO', JQTWEET.timeAgo(data[i].created_at) )
+							.replace(/ID/g, data[i].id_str)
+					);
+					}
+			}
 
-    },
+		});
 
+	},
+	/**
+	* relative time calculator FROM TWITTER
+	* @param {string} twitter date string returned from Twitter API
+	* @return {string} relative time like "2 minutes ago"
+	*/
+	timeAgo: function(dateString) {
+		var rightNow = new Date();
+		var then = new Date(dateString);
+		var diff = rightNow - then;
+		var second = 1000;
+		var minute = second * 60;
+		var hour = minute * 60;
+		var day = hour * 24;
 
-    /**
-      * relative time calculator FROM TWITTER
-      * @param {string} twitter date string returned from Twitter API
-      * @return {string} relative time like "2 minutes ago"
-      */
-    timeAgo: function(dateString) {
-        var rightNow = new Date();
-        var then = new Date(dateString);
+		if ($.browser.msie) {
+			// IE can't parse these crazy Ruby dates
+			then = Date.parse(dateString.replace(/( \+)/, ' UTC$1'));
+		}
 
-        if ($.browser.msie) {
-            // IE can't parse these crazy Ruby dates
-            then = Date.parse(dateString.replace(/( \+)/, ' UTC$1'));
-        }
+		if (isNaN(diff) || diff < 0) {
+				return ''; // return blank string if unknown
+			}
+			if (diff < second * 2) {
+				// within 2 seconds
+				return 'right now';
+			}
+			if (diff < minute) {
+				return Math.floor(diff / second) + ' seconds ago';
+			}
+			if (diff < minute * 2) {
+				return 'about 1 minute ago';
+			}
+			if (diff < hour) {
+				return Math.floor(diff / minute) + ' minutes ago';
+			}
+			if (diff < hour * 2) {
+				return 'about 1 hour ago';
+			}
+			if (diff < day) {
+				return  Math.floor(diff / hour) + ' hours ago';
+			}
+			if (diff > day && diff < day * 2) {
+				return 'yesterday';
+			}
+			if (diff < day * 365) {
+				return Math.floor(diff / day) + ' days ago';
+			}
+			else {
+				return 'over a year ago';
+			}
+		},
+		/**
+		* The Twitalinkahashifyer!
+		* http://www.dustindiaz.com/basement/ify.html
+		* Eg:
+		* ify.clean('your tweet text');
+		*/
+		ify:  {
+			link: function(tweet) {
+				return tweet.replace(/\b(((https*\:\/\/)|www\.)[^\"\']+?)(([!?,.\)]+)?(\s|$))/g, function(link, m1, m2, m3, m4) {
+				var http = m2.match(/w/) ? 'http://' : '';
+				return '<a class="twtr-hyperlink" target="_blank" href="' + http + m1 + '">' + ((m1.length > 25) ? m1.substr(0, 24) + '...' : m1) + '</a>' + m4;
+				});
+			},
 
-        var diff = rightNow - then;
+			at: function(tweet) {
+				return tweet.replace(/\B[@＠]([a-zA-Z0-9_]{1,20})/g, function(m, username) {
+				return '<a target="_blank" class="twtr-atreply" href="http://twitter.com/intent/user?screen_name=' + username + '">@' + username + '</a>';
+				});
+			},
 
-        var second = 1000,
-        minute = second * 60,
-        hour = minute * 60,
-        day = hour * 24,
-        week = day * 7;
+			list: function(tweet) {
+				return tweet.replace(/\B[@＠]([a-zA-Z0-9_]{1,20}\/\w+)/g, function(m, userlist) {
+				return '<a target="_blank" class="twtr-atreply" href="http://twitter.com/' + userlist + '">@' + userlist + '</a>';
+				});
+			},
 
-        if (isNaN(diff) || diff < 0) {
-            return ""; // return blank string if unknown
-        }
+			hash: function(tweet) {
+				return tweet.replace(/(^|\s+)#(\w+)/gi, function(m, before, hash) {
+				return before + '<a target="_blank" class="twtr-hashtag" href="http://twitter.com/search?q=%23' + hash + '">#' + hash + '</a>';
+				});
+			},
 
-        if (diff < second * 2) {
-            // within 2 seconds
-            return "right now";
-        }
-
-        if (diff < minute) {
-            return Math.floor(diff / second) + " seconds ago";
-        }
-
-        if (diff < minute * 2) {
-            return "about 1 minute ago";
-        }
-
-        if (diff < hour) {
-            return Math.floor(diff / minute) + " minutes ago";
-        }
-
-        if (diff < hour * 2) {
-            return "about 1 hour ago";
-        }
-
-        if (diff < day) {
-            return  Math.floor(diff / hour) + " hours ago";
-        }
-
-        if (diff > day && diff < day * 2) {
-            return "yesterday";
-        }
-
-        if (diff < day * 365) {
-            return Math.floor(diff / day) + " days ago";
-        }
-
-        else {
-            return "over a year ago";
-        }
-    }, // timeAgo()
-
-
-    /**
-      * The Twitalinkahashifyer!
-      * http://www.dustindiaz.com/basement/ify.html
-      * Eg:
-      * ify.clean('your tweet text');
-      */
-    ify:  {
-      link: function(tweet) {
-        return tweet.replace(/\b(((https*\:\/\/)|www\.)[^\"\']+?)(([!?,.\)]+)?(\s|$))/g, function(link, m1, m2, m3, m4) {
-          var http = m2.match(/w/) ? 'http://' : '';
-          return '<a class="twtr-hyperlink" target="_blank" href="' + http + m1 + '">' + ((m1.length > 25) ? m1.substr(0, 24) + '...' : m1) + '</a>' + m4;
-        });
-      },
-
-      at: function(tweet) {
-        return tweet.replace(/\B[@＠]([a-zA-Z0-9_]{1,20})/g, function(m, username) {
-          return '<a target="_blank" class="twtr-atreply" href="http://twitter.com/intent/user?screen_name=' + username + '">@' + username + '</a>';
-        });
-      },
-
-      list: function(tweet) {
-        return tweet.replace(/\B[@＠]([a-zA-Z0-9_]{1,20}\/\w+)/g, function(m, userlist) {
-          return '<a target="_blank" class="twtr-atreply" href="http://twitter.com/' + userlist + '">@' + userlist + '</a>';
-        });
-      },
-
-      hash: function(tweet) {
-        return tweet.replace(/(^|\s+)#(\w+)/gi, function(m, before, hash) {
-          return before + '<a target="_blank" class="twtr-hashtag" href="http://twitter.com/search?q=%23' + hash + '">#' + hash + '</a>';
-        });
-      },
-
-      clean: function(tweet) {
-        return this.hash(this.at(this.list(this.link(tweet))));
-      }
-    } // ify
-
-
-};
-
+			clean: function(tweet) {
+				return this.hash(this.at(this.list(this.link(tweet))));
+			}
+		}
+	};
 	// start jqtweet!
 	JQTWEET.loadTweets();
 
 
 });
-
-
-/*! A fix for the iOS orientationchange zoom bug.
- Script by @scottjehl, rebound by @wilto.
- MIT / GPLv2 License.
-*/
-(function(w){
-
-  // This fix addresses an iOS bug, so return early if the UA claims it's something else.
-  var ua = navigator.userAgent;
-  if( !( /iPhone|iPad|iPod/.test( navigator.platform ) && /OS [1-5]_[0-9_]* like Mac OS X/i.test(ua) && ua.indexOf( "AppleWebKit" ) > -1 ) ){
-    return;
-  }
-
-    var doc = w.document;
-
-    if( !doc.querySelector ){ return; }
-
-    var meta = doc.querySelector( "meta[name=viewport]" ),
-        initialContent = meta && meta.getAttribute( "content" ),
-        disabledZoom = initialContent + ",maximum-scale=1",
-        enabledZoom = initialContent + ",maximum-scale=10",
-        enabled = true,
-    x, y, z, aig;
-
-    if( !meta ){ return; }
-
-    function restoreZoom(){
-        meta.setAttribute( "content", enabledZoom );
-        enabled = true;
-    }
-
-    function disableZoom(){
-        meta.setAttribute( "content", disabledZoom );
-        enabled = false;
-    }
-
-    function checkTilt( e ){
-    aig = e.accelerationIncludingGravity;
-    x = Math.abs( aig.x );
-    y = Math.abs( aig.y );
-    z = Math.abs( aig.z );
-
-    // If portrait orientation and in one of the danger zones
-        if( (!w.orientation || w.orientation === 180) && ( x > 7 || ( ( z > 6 && y < 8 || z < 8 && y > 6 ) && x > 5 ) ) ){
-      if( enabled ){
-        disableZoom();
-      }
-        }
-    else if( !enabled ){
-      restoreZoom();
-        }
-    }
-
-  w.addEventListener( "orientationchange", restoreZoom, false );
-  w.addEventListener( "devicemotion", checkTilt, false );
-
-})( this );
-
 
