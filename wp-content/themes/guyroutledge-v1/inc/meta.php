2a4aef1a2fe50f6fb784c7d05b14a775b2959300
Guy Routledge <guy@bigfish.co.uk>
2013-10-12 21:45:47 +0100

    Add link to Google plus and rel=author for search results

--- wp-content/themes/guyroutledge-v1/inc/meta.php
+++ wp-content/themes/guyroutledge-v1/inc/meta.php
@@ -1,2 +1,2 @@
-<small class="post-meta">By <span class="author"><?php the_author() ?></span><br>
+<small class="post-meta">By <a href="https://plus.google.com/105864067956325551667" rel="author"><span class="author"><?php the_author() ?></span></a><br>
 lovingly typed on <?php the_time('jS F Y') ?> and filed under <?php the_category(', ') ?></small>
